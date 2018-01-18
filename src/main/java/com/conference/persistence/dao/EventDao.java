56fd26f29db4bd257a2625a8c16f027e3904c93c
Gleb <ggleb203@gmail.com>
2018-01-18 10:45:07 +0200

    Update EventDao

--- src/main/java/com/conference/persistence/dao/EventDao.java
+++ src/main/java/com/conference/persistence/dao/EventDao.java
@@ -46,16 +46,20 @@ public class EventDao extends AbstractDao<Event, Long> {
 
     @Override
     public String getCreateQuery() {
-        return "INSERT INTO event (e_date, e_time) VALUES(?, ?);";
+        return "insert into event (e_date, e_time) values(?, ?);";
     }
 
     public String getCreateQuerySecond() {
-        return "INSERT INTO "+language+" (event_id, topic, place) VALUES(last_insert_id(), ?, ?);";
+        return "insert into "+language+" (event_id, topic, place) values(last_insert_id(), ?, ?);";
     }
 
     @Override
     public String getUpdateQuery() {
-        return "update event, "+language+" set topic = ?, place = ?, e_date = ?, e_time = ? where event.id = "+language+".event_id and id = ?;";
+        return "update event set e_date = ?, e_time = ? where id = ?;";
+    }
+
+    public String getUpdateQuerySecond() {
+        return "update "+language+" set topic = ?, place = ? where event_id = ?;";
     }
 
     @Override
@@ -109,11 +113,21 @@ public class EventDao extends AbstractDao<Event, Long> {
     protected void prepareStatementForUpdate(PreparedStatement statement,
                                              Event object) throws PersistException {
         try {
+            statement.setDate(1, new Date(object.getCalendar().getTimeInMillis()));
+            statement.setTime(2, new Time(object.getCalendar().getTimeInMillis()));
+            statement.setLong(3, object.getId());
+        } catch (Exception e) {
+            LOG.error("Exception: ", e);
+            throw new PersistException(e);
+        }
+    }
+
+    protected void prepareStatementForUpdateSecond(PreparedStatement statement,
+                                             Event object) throws PersistException {
+        try {
             statement.setString(1, object.getTopic());
             statement.setString(2, object.getPlace());
-            statement.setDate(3, new Date(object.getCalendar().getTimeInMillis()));
-            statement.setTime(4, new Time(object.getCalendar().getTimeInMillis()));
-            statement.setLong(5, object.getId());
+            statement.setLong(3, object.getId());
         } catch (Exception e) {
             LOG.error("Exception: ", e);
             throw new PersistException(e);
@@ -168,5 +182,28 @@ public class EventDao extends AbstractDao<Event, Long> {
             throw new PersistException(e);
         }
         return persistInstance;
+    }
+
+    public void update(Event object) throws PersistException {
+        String sql = getUpdateQuery();
+        String sql2 = getUpdateQuerySecond();
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             PreparedStatement statement2 = connection.prepareStatement(sql2)) {
+            connection.setAutoCommit(false);
+            prepareStatementForUpdate(statement, object);
+            int count = statement.executeUpdate();
+            if (count != 1) {
+                throw new PersistException("On update modify more then 1 record: " + count);
+            }
+            prepareStatementForUpdateSecond(statement2, object);
+            int count1 = statement.executeUpdate();
+            if (count1 != 1) {
+                throw new PersistException("On update modify more then 1 record: " + count);
+            }
+            connection.commit();
+        } catch (Exception e) {
+            LOG.error("Exception: ", e);
+            throw new PersistException(e);
+        }
     }
 }
